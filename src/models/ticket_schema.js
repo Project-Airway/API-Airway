f23c44115d386ce0d342fb5c45f3285269caaa12
AneeshDixith <84792428+AneeshDixith@users.noreply.github.com>
2021-11-24 21:50:24 +0530

    Create ticket_schema.js
    Added schema for tickets.

---
+++ src/models/ticket_schema.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const ticket_schema = mongoose.Schema({
+
+    pnr : {
+        type : Number,
+        required : true
+    },
+
+    flight_id : {
+        type : String,
+        required : true
+    },
+
+    source : {
+        type : String,
+        required : true
+    },
+
+    dest_ : {
+        type : String,
+        required : true
+    },
+
+    date_of_travel : {
+        type : Date,
+        required : true
+    },
+
+    
+    number_of_seats : {
+        type : true,
+        required : true
+    }
+    
+});
+
+mongoose.exports = mongoose.model('Tickets', ticket_schema);
